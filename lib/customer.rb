f247b1cd93f2ad3a7f220391c65c58554aecc0c3
Michael Carroll <carrollm2@gmail.com>
2019-11-09 19:14:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,10 +19,10 @@ class Customer
 
   def new_meal(waiter, total, tip)
     new_meal = Meal.new()
-    new_meal.waiter = waiter
-    new_meal.customer = self
-    new_meal.total = total
-    new_meal.tip = tip
+    @new_meal.waiter = waiter
+    @new_meal.customer = self
+    @new_meal.total = total
+    @new_meal.tip = tip
     @meals << new_meal
     @waiters << new_meal.waiter
   end
