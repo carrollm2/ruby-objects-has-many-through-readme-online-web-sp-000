815b2b49d92778ebd6e7c96ddbb51cd8278f1884
Michael Carroll <carrollm2@gmail.com>
2019-11-09 17:59:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,8 +6,8 @@ class Waiter
 
   def initialize(name, years_of_experience)
     @name = name
-    @years_of_experience = years_of_experience  
-    @@all << self  
+    @years_of_experience = years_of_experience
+    @@all << self
   end
 
   def self.all
