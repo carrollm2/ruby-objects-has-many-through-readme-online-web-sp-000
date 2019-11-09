fb394d9724c01762faf93fce88ddfe7e550bb26e
Michael Carroll <carrollm2@gmail.com>
2019-11-09 18:11:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -8,10 +8,20 @@ class Waiter
     @name = name
     @years_of_experience = years_of_experience
     @@all << self
+    @meals = []
   end
 
   def self.all
     @@all
+  end
+
+  def new_meal
+    new_meal = Meal.new(self, customer, total, tip)
+    @meals << self
+  end
+
+  def best_tipper
+
   end
 
 end
